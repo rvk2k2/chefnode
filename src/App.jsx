5347b85cd4b61c7487e89b8b4ee3c117e23d3301
rvk2k2 <rahulvk2002@gmail.com>
2025-06-16 19:06:28 +0530

    chore(init): initial project setup with Vite, React, and TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-muted text-white p-6 rounded-lg">
+      Hello World
+    </div>
+  );
+}
+
+export default App
