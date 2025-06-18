8d05a6d6413c45b21ba5678d2e15f7495939b3b9
rvk2k2 <rahulvk2002@gmail.com>
2025-06-19 00:04:18 +0530

    feat(core): add navbar component and configure client-side routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import HomePage from './pages/HomePage'
 import { Routes,Route } from 'react-router-dom';
 import RecipeDetailPage from './pages/RecipeDetailPage';
 import FavoritesPage from './pages/FavoritesPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+    <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
-      <Route path="/recipes/:id" element={<RecipeDetailPage />} />
       <Route path="/favorites" element={<FavoritesPage />} />
+      <Route path="/recipes/:id" element={<RecipeDetailPage />} />
     </Routes>
+    </>
   );
 }
 
